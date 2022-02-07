a90d4fca82fd952f4b91e83dfd319e00407531a5
AndrewThompson1998 <andrew.thompson2@wsu.edu>
2022-02-07 11:58:42 -0800

    <iframe> UI attempt
    Work on TEMP folder for <iframe> UI

---
+++ TEMP/scripts/streamSource.js
@@ -0,0 +1,76 @@
+//STRINGS FOR STREAMS USING THE CRTL KEY - STREAMLINER() FUNCTION
+
+//STREAMS FROM 'DONE I'
+
+thea_s_war = new Array("Thea_s_War","Unreal","Buildup","Games","Big_Games","Unseen","Face_the_Wall","Sisters","Keep_the_Change","Thea_Harley","Heirloom","Our_Mother","Lucy_Reasons","Rock_of_Faith","Last_Days","Your_Generation","Resistance_","Futility")
+
+emily_to_thea = new Array("Emily_to_Thea","My_Fault","Get_It_Done","Opinions","Who_s_Out_of_It_","The_General","Believer","Conquering_Hero","Forbidden_Planet","Aliens","Inversion","I_m_OK","New_Kids","Love_Emily")
+
+postwar = new Array("Postwar","Morning","Noon","Night","Aftermath","Breakfast","Lunch","Dinner","Leroy_at_the_Beach","New_Drugs","Reality_Artist","Beautiful_People","At_Work","At_Play","In_Dreams","Surprise_Call","Long_Distance","Foursome","NJ","Epithalamion","About_the_War","Kisses","Then","And_Now","And_Then_Again","Mourning","Miles","Stranger","And_","Anima","Image")
+
+scare = new Array("Scare","Reprimand","Shaky","Thea_s_Mail","Poor_Correspondent","Insidious","Motives","What_About_Boris_","Chances","Bad","This_Is_a_Recording","Ratfucking","Transcript","Hostage","Apolitical","Solid_Foundation")
+
+informant = new Array("Informant","Academic","No_Paranoid_He","Naming_Names","Serious","No_Thanks","Posterior","Exhuming_McCarthy","Unreliable","Testify_Memory","Heavy","Managing")
+
+latticework = new Array("Latticework","Hi_Mom","Leroy","Thirsty","Full_of_Surprises","Truant","On_the_Road","Refuge_from_the_Road","Into_the_Country","Talking_Politics","What_s_It_Mean_","Your_Old_Man","Misguided_Dangerous","Frank_Disclosure","Edible","Frank Opinion","Rooting_for_the_Iceberg","Belief_System","Ancient_History","Radical_Mom","Not_a_War_Story","Violent_Mistake","Out_of_School","BMTV","Post-60_s","Bildung","On_Schedule","What_Should_I_Do_","Old_Movies","Blood_Aggression","Cyclops","Justice_","Remember_Your_Roots","Hustler","Rebel Roots","No Excuses","Doing_My_Best","Streets","Forget_Politics")
+
+demo = new Array("Demo","Here_They_Come","Critics","Special_Occasion","A_Close_One","Say_Miss_","Loose_Lips","Statement","Joker","Fancy_This","Was_That_You_","What_If_It_Was_","Keep_Smiling","Not_So_Cool","Object_Lesson","All_the_Way","Where_Troubles_Melt","Uh_Oh_","_But_No")
+
+dear_emily = new Array("Dear_Emily","No_Resistance","Failure","War_Games","Hopeless","Firm","Same_Old_Emily","Sundress","Suit_You_","Otherness","Playing_Around","Look_in_the_Mirror","Women_s_Issue","Come_Home")
+
+telephone = new Array("Telephone","Ringing_Tone","Listeners","Caller","Unhappy","Adjectival","Business","Proposition","Courteous","Demand","Letters","Two_or_Three_Thousand","People_Power","Why_Bother_","String_Attached","Unity_Candidate","Bad_Shit","Background_Fill","Ask_the_Man")
+
+//STREAMS FROM 'DONE II'
+
+u_turns = new Array("U_Turns","Not_Right","What_Am_I_","A_Little_Paranoia","Five_Hundred","Reflex","What_U_Know","Engineer","No_Complaints","Broadcast","The_White_Suit","Create","For Me_For You_","Ubi_Sunt","Know_About_That","Vast_Perpetual","A_New_Refutation_of_Time","Turnabout","About_Turn")
+
+whitman_s_creek = new Array("Whitman_s_Creek","Meet_the_Creek","Anomaly","Miles_in_Nature","Coming_In_","Splashdown","While_You_Can","Fertile_Subject","Up_the_Creek","With_a_Bullet","Et_Voila","Something_Happens","Well_in_Hand","The_Prophet_Returns")
+
+ten_thousand_heroes = new Array("10_000_Heroes","Our_Heroes","Absent_Brothers","Going_West","Decline_of_Man","Doubly_Incorrect","Speaking_of_U_","Parley","Floor_Thirteen","Newsworthy","Talking_Tate","What_s_With_Boris_","All_or_None","Now_Realize_This","No_Heroes","Anger","Guess_Who_","No_One_But_U_")
+
+aperture = new Array("Aperture","Wise","Not_I","How_U_","Audience","Speaking_of_Which_","How_Things_Are","Some_Kind_of_Crisis","Ourselves","Theory_of_My_Life","Multiplier_Effect","Some_Scene","The_Seventies","Trembling_on_the_Brink","On_the_Bus","What_She_Knows","Better_All_The_Time","Distinctions")
+
+veronica_s_party = new Array("Veronica_s_Party","Ghost","Body_Shots","Resistance_Begins","Polemicist","Exchange_of_Views","Conflict_Management","Public_Opinion","Check_It_Out","Backyard","Harangue","Prophet")
+
+visitor = new Array("Visitor","Face_to_Face","Open_Up","Tricky","Trigger","Ha_Ha","Query","Who_s_More_Paranoid_","Suspicions","Your_Input","Execute","Move","Imperative","Pursuit")
+
+timely = new Array("Timely","Otherworldly","Worlds_Collide","Mail","1963_Facts","1975_Facts","1990_Facts","Dear_You","Blood_Guts_in_S_A_","Facial_Hair","Closer_Look","Face_It","Frenzy","Feed","Update","Apocalypso","Flight_Deterrence","Disasterman","The_Planes","Equation","Dream","Complicated","Gathering","No_Return","Reassurance","Do_Something","Something")
+
+l_etat_de_tate = new Array("L_Etat_de_Tate","Responsive","on_2","off_1","on_1","off","on","Ring","Help_Mister_Wizard","Ring_Cycle","Errant_Fool","Retreat","Group_Shot","In_Need_of_Help","Ring_Around","Arrant_Fool","Temple","Shooters","Helpful","Fool_s_Errand","Dome","Memorabilia","Relic","Very_Sad","Last_Words","Postindustrial","Shadows","Hold_That_Thought","Virt_","We_Can_Work_It_Out","Don_t_Forget","Forget","Mad","Faceoff","So_Stated","Interesting","Delicate","Interrupt","Th","e_re","st","is","un","re","al","_i","ns","ig","ni","fi","ca","n","t_","more_to_this_")
+
+fugitive = new Array("Fugitive","Wait_Watch","Answer_This","Elusive","Stop_and_Go","Take_Off","Line_of_Flight","No_Obstructions","Time_and_Violence","Nerves","At_the_No","Passenger","Among_Friends","Moving_Violation","Car_Chase","Sorry","Regrets","Our_American_Way_of_Life","Gas_","Emily","Hold","What_Do_I_Say_")
+
+//STREAMS FROM 'DONE III'
+
+john_law = new Array("John_Law","Maddening","Just_Do_It","Celebrity","Legendary","Mistaken_Identity","Colleagues","Cybercrimes","10_59","Even_Up","Rush","Blood","Questions","Rolling_Stock","Don_t_Worry")
+
+halftones = new Array("Halftones","Glorious","Sticky","Sea_Me","Remember_Winter","Dreamwork","It_ll_Come_to_Me","Hippie_Chick","Beige","Sister_Emily","Scary_Indeed","Peace_Now","Not_Now","Stringer","No_Go")
+
+see_no_evil = new Array("See_No_Evil","Story_of_Your_Career","Bounty","Leave_It","Far_From_It","Have_a_Dream","Mo_Pomo","With_Open_Eyes")
+
+capitol = new Array("Capitol","Eyewitness","Back_in_the_Saddle","March","Encounter","Lunch_Hour","Screwups","Comrades","Where_s_the_Story_","Tough","Which_Dream","Happy_Birthday")
+
+jsn = new Array("JSN","Lost","Look_Out_","Who_s_This_Now_","Don_t_Look","No_Music","Pass_It_On","Fog","Her_Element","Compulsion","Hard_Starter","The_Dance","Act_Up","Veronica_s_Fall","Exit")
+
+in_the_no = new Array("In_the_No","Nice_Girls","Adoration","Listen_Up","Stone_in_Love","Irrational_Heart","Unfit","Rose_Tattoo","Something_Wild","Thanks_for_the_Advice","Bitchin_","Age_Before_Bounty","Seriously","More_of_the_Same","Dear_Victor")
+
+//STREAMS FROM 'DONE IV'
+
+slacktown = new Array("Slacktown","The_LaST_Machine","Parabolic","Monastic","Ecumenical","Inspirational","Supplementary","Diabolic","Tumble","Free","Om","Again_","CPUSA","Fuel_Situation","This_is_War","Ready_for_Delivery","Dropkick","Wallshot","Nominal","Facemask","Cover_Story","Plots_Thicken","Things_Change","Great_Hall","Crash","Miles_Miles","Eat","Amulet","Microdot","What_They_Say","What_They_REALLY_Say","Talking_With_Your_Hands","Code_42","Really_Guys","Letter_Man","Treadmill","Devoted","Drawing_Room","Mistaken","Tate_Reboots","New","World","Leader")
+
+now_here_this = new Array("Now_Here_This","Initial_Conditions","Watch_My_Back","National_Dick","Name_That_Fear","This_Is_It_","Talkin_bout_the_Horror","_disClamor_","Bird_Fiver-Two","Grounded","Meet_Again","Not_Yet_Implemented","P_C_","You_Say_You_Vanna","Except_","Political_Correction","Millennial","Back_to_the_Future")
+
+eighty_eighty_eight = new Array("8088","Absence_of_Boris","Impostor","The_Text","Foregone_Conclusion","Parable","Patron","Wanker","Correct_Yourself","About_Time","With_Mirrors","Mirrors_on_the_Ceiling","Plain_Enough","Work")
+
+observer = new Array("Observer","Noted_in_Passing","Stargazing","Class_Trip","Inside_Tate","Quiz","Plane_Geometry","Cometary","Plane","What_He_Could_See","Perigee","Patterns","Coincidence","Pragmatics","Conservative","Important","Serious_Business","Warning","Close")
+
+showtime = new Array("Showtime","East","West","Envax_Scamdee","Controversial","I_D_","Loop","Hyperspace","All_the_Above","Slack","Gas_Gas_Gas","Now_I_Understand","Gad","Endgame","Exeunt","Silence","The_Red_King","Into_the_Dark","Impressed","Forfeit","Just_Say_No","Vox_Pop_","Grass")
+
+dreamtime = new Array("Dreamtime","Flounder","Leviaplane","13-F","Blessed_Assurance","Wheel","O_Wow","Kid_Stuff","In_Control","Line_of_Command","Negative_Function","Emergency","Blowout","Escapade","See_Hear","Fire_in_the_Holo","Sinking_Feeling","Into_the_Wreck","Book_Him","Bed","Going_UP_","Perfectly_Sensible","The_Watchtower","Watchman","Angelology","Charlie_Tuna","Solsbury_Hill","Max-Q","Apical","Special_Delivery","Rain","En-dedans_le_Deluge","Floater","Embarked","Fishy","Dolphins_Aren_t_Fish","Rainbow","So_What","zero_point_zero","__4")
+
+//STREAMS FROM 'DONE V'
+
+//STREAMS FROM 'DONE VI'
+
+
+allStreams = new Array(thea_s_war,emily_to_thea,postwar,scare,informant,latticework,demo,dear_emily,telephone,u_turns,whitman_s_creek,ten_thousand_heroes,aperture,veronica_s_party,visitor,timely,l_etat_de_tate,fugitive,john_law,halftones,see_no_evil,capitol,jsn,in_the_no,slacktown,now_here_this,eighty_eighty_eight,observer,showtime,dreamtime)
